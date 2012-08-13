9b2dc5851a0602e74cb0b3e057937779e9fc47e6
Mark Hauenstein <mark.hauenstein@gmail.com>
2012-08-13 11:33:45 +0000

    test from pi

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Hello Mark");
   response.end();
 }).listen(8888);
